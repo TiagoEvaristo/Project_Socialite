e81fe69063cd3e8adea19540e3b929d0c42ad9c5
Tiago da Silva Evaristo <63016462+TiagoEvaristo@users.noreply.github.com>
2023-08-07 13:43:00 -0300

    Configurações iniciais do projeto

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/auth/github/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
+ 
+Route::get('/auth/github/callback', function () {
+    $user = Socialite::driver('github')->user();
+ 
+    var_dump($user);
+    // $user->token
 });
