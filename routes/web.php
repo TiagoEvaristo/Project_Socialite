be68b0513e6a0244502cec35529c937cefa594c9
Tiago da Silva Evaristo <63016462+TiagoEvaristo@users.noreply.github.com>
2023-08-09 18:13:31 -0300

    Configurado GitHub

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Laravel\Socialite\Facades\Socialite;
@@ -10,13 +11,29 @@ Route::get('/', function () {
 });
 
 
-Route::get('/auth/github/redirect', function () {
-    return Socialite::driver('github')->redirect();
+Route::get('/auth/{provider}/redirect', function (string $provider) {
+    return Socialite::driver($provider)->redirect();
 });
  
-Route::get('/auth/github/callback', function () {
-    $user = Socialite::driver('github')->user();
- 
-    var_dump($user);
-    // $user->token
+Route::get('/auth/{provider}/callback', function (string $provider) {
+    $providerUser = Socialite::driver($provider)->user();
+    
+    var_dump($providerUser);
+    // $user = User::updateOrCreate([
+    //     'github_id' => $githubUser->id,
+    // ], [
+    //     'name' => $githubUser->name,
+    //     'email' => $githubUser->email,
+    //     'github_token' => $githubUser->token,
+    //     'github_refresh_token' => $githubUser->refreshToken,
+    // ]);
+
+    // auth()->login($user, true);
+
+    // return redirect('/logged');
+    // // $user->token
 });
+
+Route::get('/logged', function () {
+    var_dump(auth()->user());
+})->middleware('auth');
